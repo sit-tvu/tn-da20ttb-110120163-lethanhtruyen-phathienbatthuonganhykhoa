1f15320c8124d6d16c12ef6559ec585b1571c9c1
truyenda20ttb <110120163@st.tvu.edu.vn>
2024-07-11 15:54:47 +0700

    add first batch of docs

---
+++ src/GiaoDien/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+from tensorflow import keras
+from keras._tf_keras.keras.models import load_model
+from keras._tf_keras.keras.preprocessing import image
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained models
+model_cnn = load_model('models/model_CNN.h5')
+model_resnet = load_model('models/model_resnet.h5')
+model_densenet = load_model('models/model_densenet.h5')
+
+def predict_label(img_path, model):
+    img = image.load_img(img_path, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+
+    # Convert the prediction to label and probability
+    labels = ['NORMAL', 'PNEUMONIA']
+    label = labels[1] if prediction > 0.5 else labels[0]
+    probability = prediction[0][0] if prediction > 0.5 else 1 - prediction[0][0]
+
+    return label, probability
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    img_path = None
+    results = {}
+    file_name = None
+    if request.method == 'POST':
+        img = request.files['image']
+        file_name = secure_filename(img.filename)
+        img_path = "static/" + file_name
+        img.save(img_path)
+
+        for model_name, model in zip(["CNN", "ResNet", "DenseNet"], [model_cnn, model_resnet, model_densenet]):
+            label, probability = predict_label(img_path, model)
+            results[model_name] = {"label": label, "probability": probability}
+        
+    return render_template("index.html", results=results, img_path=img_path, file_name=file_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
